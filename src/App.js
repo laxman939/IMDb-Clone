3538b6775f90c885635199d1125278cbfa56235b
laxman <ramlaxman947@gmail.com>
2022-02-28 19:16:46 +0530

    Js Files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CarouselComponet from "./Components/Carousel/Carousel";
+import Favourites from "./Components/Favourites/Favourites";
+import Trending from "./Components/Movies/Trending";
+import NavBar from "./Components/Navigation/NavBar";
+import Pagination from "./Components/Pagination/Pagination";
+import "./styles.css";
 
-import Reducer from "./Reducers/Index";
-import thunk from "redux-thunk";
+function App() {
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <CarouselComponet />
+              <Trending />
+              <Pagination />
+            </>
+          }
+        />
+        <Route path="/favourites" element={<Favourites />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-export default createStore(Reducer, composeEnhancers(applyMiddleware(thunk)));
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./assets/main.css";
 
 import { Provider } from "react-redux";
 import store from "./Redux/Store";
 
 // Bootstrap styles
 import "bootstrap/dist/css/bootstrap.min.css";
+
+// Antd
+import "antd/dist/antd.css";
 
 ReactDOM.render(
   <React.StrictMode>
