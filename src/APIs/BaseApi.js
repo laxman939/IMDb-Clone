0df4a3611094b9aaf4f4e8021cf92afc3cd19265
laxman <ramlaxman947@gmail.com>
2022-03-01 17:12:12 +0530

    Genre Ids

--- src/APIs/BaseApi.js
+++ src/APIs/BaseApi.js
@@ -7,3 +7,25 @@ const BaseApi = Axios.create({
 export default BaseApi;
 
 export const API_KEY = "d7a3fef03d316da2d557be3ba83065a1";
+
+export const movieGenreids = {
+  28: "Action",
+  12: "Adventure",
+  16: "Animation",
+  35: "Comedy",
+  80: "Crime",
+  99: "Documentary",
+  18: "Drama",
+  10751: "Family",
+  14: "Fantasy",
+  36: "History",
+  27: "Horror",
+  10402: "Music",
+  9648: "Mystery",
+  10749: "Romance",
+  878: "Sci-Fi",
+  10770: "TV",
+  53: "Thriller",
+  10752: "War",
+  37: "Western",
+};
