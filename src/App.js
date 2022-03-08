c4694425133a0c76f1f4f38e6f7dbc13f573f76f
laxman <ramlaxman947@gmail.com>
2022-03-08 22:24:14 +0530

    Modified code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CarouselComponet from "./Components/Carousel/Carousel";
+import ErrorPage from "./Components/ErrorPage";
 import Favourites from "./Components/Favourites/Favourites";
 import Trending from "./Components/Movies/Trending";
 import NavBar from "./Components/Navigation/NavBar";
 import Pagination from "./Components/Pagination/Pagination";
 import SignIn from "./Components/SignIn/SignIn";
 import SignUp from "./Components/SignUp/SignUp";
+import Success from "./Components/Success";
 import "./styles.css";
 
 function App() {
@@ -18,7 +20,7 @@ function App() {
           path="/"
           element={
             <>
-              <CarouselComponet />
+              {/* <CarouselComponet /> */}
               <Trending />
               <Pagination />
             </>
@@ -27,8 +29,9 @@ function App() {
         <Route path="/favourites" element={<Favourites />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/success" element={<Success />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
-      n
     </BrowserRouter>
   );
 }
