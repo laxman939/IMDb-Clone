7c0bb6eb5404ceb35c514e74d6c9930c57de3a9d
laxman <ramlaxman947@gmail.com>
2022-03-11 00:47:25 +0530

    Pagination Component

--- src/Components/Pagination/Pagination.js
+++ src/Components/Pagination/Pagination.js
@@ -1,63 +1,29 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+
 import { pageNumberAction } from "../../Redux/Actions/Actions";
+import { Pagination } from "antd";
+
 import Top from "./Top";
 
 export default function Pages() {
-  const pageNmbr = useSelector((state) => state.Page.page);
+  // const pageNmbr = useSelector((state) => state.Page.page);
   const dispatch = useDispatch();
 
   return (
-    <div className="flex md:justify-between text-center">
-      <div
-        className="pagination-section w-full 
-        flex justify-center
-        py-8 bg-gray-600"
-      >
-        <button
-          className="
-            p-2
-            border-2
-            border-red-900
-            text-gray-500
-            bg-gray-400
-            border-r-0
-            rounded-l-xl
-            hover:bg-gray-700 text-white font-medium"
-          onClick={() =>
-            pageNmbr > 1 && dispatch(pageNumberAction(pageNmbr - 1))
-          }
-        >
-          Previous
-        </button>
-        <button
-          className="
-          p-2
-          px-3
-            border-t-2 border-b-2
-            border-red-900
-            text-yellow-400 font-extrabold text-lg
-            hover:bg-gray-400"
-        >
-          {pageNmbr}
-        </button>
-        <button
-          className="
-            p-2
-            border-2
-            border-red-900
-            font-medium
-            bg-gray-400
-            border-r-0
-            rounded-r-xl
-            hover:bg-gray-700 text-white"
-          onClick={() => dispatch(pageNumberAction(pageNmbr + 1))}
-        >
-          Next
-        </button>
+    <div className="">
+      <div className="flex justify-center text-center">
+        <Pagination
+          className="text-lg bg-stone-500 text-yellow-900 p-2 rounded-lg"
+          total={10000}
+          showQuickJumper
+          defaultPageSize={10}
+          onChange={(e) => dispatch(pageNumberAction(e))}
+        />
       </div>
-
-      <Top />
+      <div className="flex justify-end text-center pb-4">
+        <Top />
+      </div>
     </div>
   );
 }
